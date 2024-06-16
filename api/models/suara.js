3526f3cf11bae9c1ed25babd468f056d4c84c5cb
will1001 <wilirahmatm@gmail.com>
2024-06-16 16:22:28 +0800

    feat - EP suara

---
+++ api/models/suara.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const suaraSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  grup_suara: { type: Number, required: true },
+  category_suara: { type: Number, required: true },
+  sub_category_suara: { type: Number, required: true },
+  laki_laki: { type: Number, required: true },
+  perempuan: { type: Number, required: true },
+  jumlah: { type: Number, required: true },
+});
+
+const suara = mongoose.model("suara", suaraSchema);
+
+module.exports = suara;
---
+++ api/routes/Suara.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const Suara = require("../models/suara");
+
+const handleServerError = (err, res) => {
+  console.error(err.message);
+  res.status(500).send("Server error");
+};
+
+router.get("/suara", async (req, res) => {
+  try {
+    const { grup_suara, category_suara, sub_category_suara } = req.query;
+
+    let filter = {};
+    if (grup_suara) filter.grup_suara = grup_suara;
+    if (category_suara) filter.category_suara = category_suara;
+    if (sub_category_suara) filter.sub_category_suara = sub_category_suara;
+
+    const suara = await Suara.find(filter);
+    res.json(suara);
+  } catch (err) {
+    handleServerError(err, res);
+  }
+  router.put("/suara/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const updateFields = [
+        "grup_suara",
+        "category_suara",
+        "sub_category_suara",
+        "laki_laki",
+        "perempuan",
+        "jumlah",
+      ];
+      let updateData = {};
+
+      updateFields.forEach((field) => {
+        if (req.body[field] !== undefined) {
+          updateData[field] = req.body[field];
+        }
+      });
+
+      const updatedSuara = await Suara.findByIdAndUpdate(id, updateData, {
+        new: true,
+      });
+      if (!updatedSuara) {
+        return res.status(404).send("Suara not found");
+      }
+      res.json(updatedSuara);
+    } catch (err) {
+      handleServerError(err, res);
+    }
+  });
+});
+
+module.exports = router;
